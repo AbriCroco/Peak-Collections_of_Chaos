0e605bc1a808cde652a524f2cb28895441f99488
AbriCroco <sivierouland@pm.me>
2025-11-24 22:52:14 +0100

    Add files via upload

---
+++ Chaos/Networking/EffectIds.cs
@@ -0,0 +1,11 @@
+namespace Chaos.Networking
+{
+    public static class EffectIds
+    {
+        public const byte None = 0;
+        public const byte Cleanse = 1;
+        public const byte Boost = 2;
+        public const byte Hunger = 3;
+        public const byte Scoutmaster = 4;
+    }
+}
